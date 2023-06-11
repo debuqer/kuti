c31cc8a409bb4475c140440442d57859480ef1f0
Mohammad Abbasi <m.abbasi@dadehpardaz.com>
2023-06-11 14:35:20 +0330

    init package

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/urfave/cli"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "Kuti"
+	app.Usage = "Build your fast and secure hacker blog"
+	app.Action = func(c *cli.Context) error {
+		fmt.Println("boom! I say!")
+		return nil
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
