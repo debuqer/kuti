5c51828cce916362d9600f5e1a896107534781db
Mohammad Abbasi <m.abbasi@dadehpardaz.com>
2023-06-11 14:51:08 +0330

    can re-create config.yml

--- main.go
+++ main.go
@@ -17,6 +17,30 @@ func main() {
 		return nil
 	}
 
+	app.Commands = []cli.Command{
+		{
+			Name:    "init",
+			Aliases: []string{"i"},
+			Usage:   "Initialize a new kuti project",
+			Action: func(c *cli.Context) error {
+				if c.Args().First() == "" {
+					fmt.Println("Project name is invalid")
+					return nil
+				}
+
+				configTpl, err := os.ReadFile("assets/config.yml")
+				if err != nil {
+					fmt.Println("Template file assets/config.yml not found")
+					return nil
+				}
+				os.WriteFile("config.yml", configTpl, 0644)
+
+				fmt.Println("Initialized a new kuti project: ", c.Args().First())
+				return nil
+			},
+		},
+	}
+
 	err := app.Run(os.Args)
 	if err != nil {
 		log.Fatal(err)
