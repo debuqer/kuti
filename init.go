778c682a4f073fcaffa4f24456893bd9cbec6755
Mohammad Abbasi <m.abbasi@dadehpardaz.com>
2023-06-24 09:30:05 +0330

    better text interface

--- init.go
+++ init.go
@@ -11,21 +11,22 @@ func InitCommand(_conf Config) cli.Command {
 	return cli.Command{
 		Name:    "init",
 		Aliases: []string{"i"},
-		Usage:   "Initialize a new kuti project",
+		Usage:   "Initialize a new KUTI project",
 		Action: func(c *cli.Context) error {
 			if c.Args().First() == "" {
-				fmt.Println("Project name is invalid")
+				fmt.Println("Please provide a valid project name")
 				return nil
 			}
 
 			configTpl, err := os.ReadFile("assets/config.yml.example")
 			if err != nil {
-				fmt.Println("Template file assets/config.yml not found")
+				fmt.Println("Could not init the project, assets/config.yml.example not found")
 				return nil
 			}
 			os.WriteFile("config.yml", configTpl, 0644)
+			fmt.Println("Config.yml added to project")
 
-			fmt.Println("Initialized a new kuti project: ", c.Args().First())
+			fmt.Println("Initialized a new KUTI project: ", c.Args().First())
 			return nil
 		},
 	}
