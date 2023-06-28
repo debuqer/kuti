3992f336b4bce62e01cef8f3ea6ecb1a48dcad6a
Mohammad Abbasi <m.abbasi@dadehpardaz.com>
2023-06-28 11:22:46 +0330

    panic alert

--- init.go
+++ init.go
@@ -20,13 +20,14 @@ func InitCommand() cli.Command {
 
 			configTpl, err := os.ReadFile("assets/config.yml.example")
 			if err != nil {
-				fmt.Println("Could not initialize the project, assets/config.yml.example not found")
-				return nil
+				panic("Could not initialize the project, assets/config.yml.example not found")
 			}
+
 			os.WriteFile("config.yml", configTpl, 0644)
 			fmt.Println("Config.yml added to project")
 
 			fmt.Println("Initialized a new KUTI project: ", c.Args().First())
+
 			return nil
 		},
 	}
