a62c65f5761fe6e6d6d44a49c4e6a454cbb14d6f
Mohammad Abbasi <m.abbasi@dadehpardaz.com>
2023-06-28 11:13:05 +0330

    use panic instead of log.Fatal

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"os"
 
 	"github.com/urfave/cli"
@@ -14,14 +12,10 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "Kuti"
 	app.Usage = "Build your fast and secure hacker blog"
-	app.Action = func(c *cli.Context) error {
-		fmt.Println("boom! I say!")
-		return nil
-	}
 
 	err := _conf.load()
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 
 	app.Commands = []cli.Command{
@@ -32,6 +26,6 @@ func main() {
 
 	err = app.Run(os.Args)
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 }
