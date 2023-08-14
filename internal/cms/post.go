c6ee8d0bc4e3c0917f73ac1681f89b97601922ca
Mohammad Abbasi <m.abbasi@dadehpardaz.com>
2023-08-14 15:50:29 +0330

    add method to post

--- internal/cms/post.go
+++ internal/cms/post.go
@@ -1,5 +1,11 @@
 package cms
 
+import (
+	"github.com/gomarkdown/markdown"
+	"github.com/gomarkdown/markdown/html"
+	"github.com/gomarkdown/markdown/parser"
+)
+
 type Post struct {
 	Title         string
 	Content       string
@@ -7,4 +13,22 @@ type Post struct {
 	Date          string
 	EstimatedTime int
 	Tags          []string
+}
+
+type Content struct {
+	String string
+}
+
+func (c *Content) ToHtml() []byte {
+	// create markdown parser with extensions
+	extensions := parser.CommonExtensions | parser.AutoHeadingIDs | parser.NoEmptyLineBeforeBlock
+	p := parser.NewWithExtensions(extensions)
+	doc := p.Parse([]byte(c.String))
+
+	// create HTML renderer with extensions
+	htmlFlags := html.CommonFlags | html.HrefTargetBlank
+	opts := html.RendererOptions{Flags: htmlFlags}
+	renderer := html.NewRenderer(opts)
+
+	return markdown.Render(doc, renderer)
 }
